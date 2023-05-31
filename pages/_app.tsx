2d1a5385917f81068f4aaeee6569f27f5968786f
Mahfuj Ahmed <ahmed.mahfuj02@gmail.com>
2023-06-01 01:39:34 +0600

    Build Chakra light theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
 export default MyApp;
