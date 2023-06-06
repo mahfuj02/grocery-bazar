3f65edc2efdd96bf9d1ac3e89c6248f5216d1da3
Mahfuj Ahmed <ahmed.mahfuj02@gmail.com>
2023-06-07 04:53:39 +0600

    Install magnify image for zoom picture

--- components/product_detail/ImageGallery.tsx
+++ components/product_detail/ImageGallery.tsx
@@ -1,4 +1,7 @@
-import { Image } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
+import { useState } from "react";
+import ReactImageMagnify from 'react-image-magnify';
+// import  from 'react-image-magnify'
 
 interface Props {
   image?: string;
@@ -6,7 +9,48 @@ interface Props {
 }
 
 const ImageGallery = ({ image, title }: Props) => {
-  return <Image src={image} alt={title} />;
+  const [isHovered, setIsHovered] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+
+  const handleImageHover = (event: { clientX: any; clientY: any; }) => {
+    setIsHovered(!isHovered);
+    setCursorPosition({ x: event.clientX, y: event.clientY });
+
+  };
+
+  return (
+    <Box
+    position="relative"
+    overflow="hidden"
+    onMouseMove={handleImageHover}
+    onMouseLeave={() => setIsHovered(false)}
+    >
+       <ReactImageMagnify
+        {...{
+          smallImage: {
+            alt: "Product Image",
+            isFluidWidth: true,
+            src: image,
+          },
+          largeImage: {
+            src: image,
+            width: 1500,
+            height: 800,
+          },
+          enlargedImagePosition: "beside",
+          // hoverDelayInMs: 0,
+          // hoverOffDelayInMs: 0,
+          // isHintEnabled: true,
+        }}
+      />
+      {/* <Image
+        src={image}
+        alt={title}
+      
+      /> */}
+    </Box>
+  );
 };
 
 export default ImageGallery;
